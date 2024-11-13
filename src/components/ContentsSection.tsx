73566657c1dcfcf11c1258d935ae37f379d31dda
fuerte <leandrofuerte75@gmail.com>
2024-11-13 19:05:49 +0100

    Componentizing articles

--- src/components/ContentsSection.tsx
+++ src/components/ContentsSection.tsx
@@ -1,11 +1,33 @@
+import VitalAreaCard from "@/components/VitalAreaCard";
+
+const vitalAreas = [
+  {
+    id: 1,
+    title: "Health",
+    route: "/health",
+    background: "bg-green-600",
+  },
+  {
+    id: 2,
+    title: "Social",
+    route: "/social",
+    background: "bg-red-600",
+  },
+  {
+    id: 3,
+    title: "Thrive",
+    route: "/thrive",
+    background: "bg-purple-600",
+  },
+];
+
 const ContentSection = () => {
   return (
     <section className="height-80 flex flex-col md:flex-row mdjustify-around">
-      <article className="bg-green-600 h-56 flex-grow m-5 rounded-lg"></article>
-      <article className="bg-red-600 h-56 flex-grow m-5 rounded-lg"></article>
-      <article className="bg-purple-600 h-56 flex-grow m-5 rounded-lg"></article>
+      {vitalAreas.map((area) => (
+        <VitalAreaCard area={area} />
+      ))}
     </section>
   );
 };
-
 export default ContentSection;

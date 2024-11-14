ec8946037cf60d8fb0b9ceb01e8e13d227cb7b89
fuerte <leandrofuerte75@gmail.com>
2024-11-14 16:37:12 +0100

    Quitting unnecessary comments

--- src/components/QuotesApp.tsx
+++ src/components/QuotesApp.tsx
@@ -4,7 +4,7 @@ import { QuoteCard } from "./QuoteCard";
 
 const QuotesApp: React.FC = () => {
   function getRandomNumber(max: number): number {
-    return Math.floor(Math.random() * max); // +1 para incluir el valor max
+    return Math.floor(Math.random() * max);
   }
 
   const [currentIndex, setCurrentIndex] = useState(getRandomNumber(50));
@@ -12,9 +12,9 @@ const QuotesApp: React.FC = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentIndex(getRandomNumber(50));
-    }, 10000); // Cambia cada 1000 ms
-    return () => clearInterval(intervalId); // Cleans up the interval on unmount
-  }, [Quotes.length]);
+    }, 10000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <section className="md:ml-6 inline-block w-full p-6 mt-10 mb-10 md:mb-0 md:mt-8 xl:mt-16">

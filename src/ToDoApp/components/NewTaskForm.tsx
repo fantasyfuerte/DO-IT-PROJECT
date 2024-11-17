c52d853cd2226e0866a6b0ebe0431d04a4b434d3
fuerte <leandrofuerte75@gmail.com>
2024-11-17 13:20:57 +0100

    Fixing width

--- src/ToDoApp/components/NewTaskForm.tsx
+++ src/ToDoApp/components/NewTaskForm.tsx
@@ -44,7 +44,7 @@ const NewTaskForm: React.FC<Props> = ({ dispatch, filter }) => {
         disabled={filter !== filters.ALL ? true : false}
       />
       <button
-        className="inline-block md:hidden text-white bg-gray-900 rounded-full ml-4"
+        className="inline-block md:hidden p-1 text-white bg-gray-900 rounded-full ml-1 mb-2"
         onClick={handlePress}
       >
        <PlusIcon/>

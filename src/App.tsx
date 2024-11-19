ee99a9c514f1c84ed708d1eb7be6f61e26eaca73
fuerte <leandrofuerte75@gmail.com>
2024-11-19 19:15:13 +0100

    Creating new component HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import ContentSection from "./ContentsSection/ContentsSection";
 import Nav from "./components/Nav";
-import QuotesApp from "./QuotesApp/QuotesApp";
-import ScrollDownText from "./components/ScrollDownText";
-import ToDoApp from "./ToDoApp/ToDoApp";
 import { routes } from "./constants";
+import { HomePage } from "./pages/HomePage";
+
 
 function App() {
   return (
@@ -12,12 +10,7 @@ function App() {
         <Nav routes={routes} />
       </header>
       <main>
-        <div className="flex flex-col md:flex-row mt-32 md:mt-0 md:justify-between justify-start items-center h-[83vh] md:h-screen">
-          <ToDoApp />
-          <QuotesApp />
-        </div>
-        <ScrollDownText />
-        <ContentSection />
+        <HomePage></HomePage>
       </main>
     </>
   );
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import ScrollDownText from "@/components/ScrollDownText";
+import ContentSection from "@/ContentsSection/ContentsSection";
+import QuotesApp from "@/QuotesApp/QuotesApp";
+import ToDoApp from "@/ToDoApp/ToDoApp";
+
+
+export const HomePage: React.FC = () => {
+  return (
+    <>
+      <div className="flex flex-col md:flex-row mt-32 md:mt-0 md:justify-between justify-start items-center h-[83vh] md:h-screen">
+        <ToDoApp />
+        <QuotesApp />
+      </div>
+      <ScrollDownText />
+      <ContentSection />
+    </>
+  );
+};

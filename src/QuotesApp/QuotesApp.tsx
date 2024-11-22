5454741986d826806584d374cf65309c74c0ae2a
fuerte <leandrofuerte75@gmail.com>
2024-11-22 16:53:33 +0100

    Refactoring

--- src/QuotesApp/QuotesApp.tsx
+++ src/QuotesApp/QuotesApp.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Quotes } from "../constants";
 import { QuoteCard } from "./components/QuoteCard";
-import {motion} from 'motion/react'
+import { motion } from "motion/react";
 
 const QuotesApp: React.FC = () => {
   function getRandomNumber(max: number): number {
@@ -18,7 +18,12 @@ const QuotesApp: React.FC = () => {
   }, []);
 
   return (
-    <motion.section initial={{opacity:0}} animate={{opacity:1}} transition={{delay:1.8}} className="mt-20 md:mt-0 md:ml-6 inline-block p-6 basis-3/5">
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.8 }}
+      className="mt-20 md:mt-0 md:ml-6 inline-block p-6 basis-3/5"
+    >
       <QuoteCard quote={Quotes[currentIndex]} />
     </motion.section>
   );

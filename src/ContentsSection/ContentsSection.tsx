0e86bb9869ce50e7acd2550c2e249241998243ca
fuerte <leandrofuerte75@gmail.com>
2024-11-23 18:30:47 +0100

    Styling vitalAreaCards

--- src/ContentsSection/ContentsSection.tsx
+++ src/ContentsSection/ContentsSection.tsx
@@ -6,19 +6,20 @@ const vitalAreas = [
     id: 1,
     title: "Health",
     route: "/health",
-    background: "bg-green-950",
+    color: "text-green-950",
   },
   {
     id: 2,
     title: "Social",
     route: "/social",
-    background: "bg-red-950",
+    color: "text-red-950",
   },
   {
     id: 3,
     title: "Thrive",
     route: "/thrive",
-    background: "bg-purple-950",
+    color: "text-purple-950",
+    
   },
 ];
 
--- src/ContentsSection/VitalAreaCard.tsx
+++ src/ContentsSection/VitalAreaCard.tsx
@@ -5,7 +5,7 @@ interface Props {
     title: string;
     id: number;
     route: string;
-    background: string;
+    color: string;
   };
 }
 
@@ -13,11 +13,15 @@ const VitalAreaCard: React.FC<Props> = ({ area }) => {
   return (
     <motion.article
       initial={{ opacity: 0 }}
-      transition={{ delay: (area.id * 2) / 10, duration: 0.5 }}
       whileInView={{ opacity: 1 }}
-      className={`${area.background} h-44 md:h-96 flex-grow m-5 rounded-lg flex justify-center items-center`}
+      transition={{ delay: (area.id * 2) / 10, duration: 0.5 }}
+      className={`bg-transparent h-44 md:h-96 flex-grow m-5 rounded-lg flex justify-center items-center vitalareacard `}
     >
-      <h2 className="text-7xl font-black text-white">{area.title}</h2>
+      <h2
+        className={`${area.color} text-7xl font-black`}
+      >
+        {area.title}
+      </h2>
     </motion.article>
   );
 };

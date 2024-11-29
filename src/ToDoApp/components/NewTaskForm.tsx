fd0a5bb64547ee30fee9786be0d57273a91c7e6b
fuerte <leandrofuerte75@gmail.com>
2024-11-29 17:06:32 +0100

    Manejando el formulario sin estados

--- src/ToDoApp/components/NewTaskForm.tsx
+++ src/ToDoApp/components/NewTaskForm.tsx
@@ -3,6 +3,7 @@ import { filters } from "../ToDoApp";
 import { useState } from "react";
 import { PlusIcon } from "lucide-react";
 import { motion } from "motion/react";
+import { form } from "motion/react-client";
 
 interface Props {
   dispatch: ({}: ActionDispatch) => void;
@@ -10,28 +11,23 @@ interface Props {
 }
 
 const NewTaskForm: React.FC<Props> = ({ dispatch, filter }) => {
-  const [title, setTitle] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const title = formData.get("content")?.toString();
 
-  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key !== "Enter" || title.length == 0 || title.trim() == "") return;
+    if (!title || title.length == 0 || title.trim() == "") return;
     dispatch({
       type: DispatchType.ADD,
       payload: { title, id: crypto.randomUUID() },
     });
-    setTitle("");
-  };
-
-  const handlePress = () => {
-    if (title.length == 0 || title.trim() == "") return;
-    dispatch({
-      type: DispatchType.ADD,
-      payload: { title, id: crypto.randomUUID() },
-    });
-    setTitle("");
+    const input = e.currentTarget.elements.namedItem("content");
+    if (input instanceof HTMLInputElement) input.value = "";
   };
 
   return (
-    <div
+    <form
+      onSubmit={handleSubmit}
       className={`transition duration-300 flex justify-center items-center w-full ${
         filter !== filters.ALL && "opacity-15"
       } `}
@@ -41,20 +37,17 @@ const NewTaskForm: React.FC<Props> = ({ dispatch, filter }) => {
         animate={{ opacity: 1 }}
         type="text"
         placeholder="Five most important things for today"
-        onChange={(e) => setTitle(e.target.value)}
-        onKeyDown={handleEnter}
-        value={title}
+        name="content"
         className="min-w-[300px] md:w-4/5 outline-none bg-transparent border-b-4 border-gray-300 text-center text-lg font-medium mb-2 shrink-0"
         disabled={filter !== filters.ALL ? true : false}
       />
       <motion.button
         className="inline-block md:hidden p-1 text-white bg-gray-900 rounded-full ml-1 mb-2"
-        onClick={handlePress}
         whileTap={{ scale: 1.15 }}
       >
         <PlusIcon />
       </motion.button>
-    </div>
+    </form>
   );
 };
 

598cea4b36d502cec38c8efff2a6276ee9cb4df2
fuerte <leandrofuerte75@gmail.com>
2024-11-29 14:56:32 +0100

    Styling

--- src/ContentsSection/components/StyledArticle.tsx
+++ src/ContentsSection/components/StyledArticle.tsx
@@ -31,14 +31,14 @@ const StyledArticle: React.FC<Props> = ({
       transition={{ delay: 0.3, duration: 0.2 }}
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
-      className="flex max-h-max flex-wrap items-end mb-48"
+      className="flex max-h-max flex-wrap items-end mb-36"
     >
       <div className="basis-6/12 flex-grow min-w-min pr-4 md:pr-0 pl-4 mb-7">
         <h5 className="text-5xl min-w-60">
           {title}: <br />
           <strong className="text-gray-950">{subtitle}</strong>
         </h5>
-        <p className="text-lg font-semibold mt-8 text-pretty first-letter:ml-2">
+        <p className="text-lg text-gray-800 font-semibold mt-8 text-pretty first-letter:ml-2">
           {paragraph}
         </p>
       </div>

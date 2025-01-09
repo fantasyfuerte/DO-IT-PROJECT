bb605c64a405a373c0f6a09ad3c1e6cf988cd57f
fuerte <leandrofuerte75@gmail.com>
2025-01-09 12:37:36 +0100

    Re writing and styling why this areas paragraph

--- src/ContentsSection/components/WhyThisAreas.tsx
+++ src/ContentsSection/components/WhyThisAreas.tsx
@@ -11,22 +11,31 @@ const WhyThisAreas = () => {
       >
         Why this 3 areas
       </motion.h2>
-      <motion.p
+      <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ delay: 0.7 }}
-        className="text-gray-950 text-xl mt:m-8 mt-12 text-center md:text-pretty"
+        className="text-gray-950 text-xl grid grid-cols-2 gap-10 mx-24 mt-12 text-center"
       >
-        These three areas are fundamental to a fulfilling and balanced life, and
-        are inextricably linked. They don't exist in isolation; instead, they
-        inf8uence and support each other in a complex interplay.
-        <br />
-        Improvements in one area often create a positive ripple effect, boosting
-        progress in the others.
-        <br />A holistic strategy that acknowledges and addresses the
-        synergistic relationships between these three key areas is the most
-        effective path towards sustainable and comprehensive well-being.
-      </motion.p>
+        <p>
+          These three areas are fundamental to a fulfilling and balanced life,
+          and are inextricably linked. They don't exist in isolation; instead,
+          they influence and support each other in a complex interplay.
+        </p>
+        <p>
+          Improvements in one area often create a positive ripple effect,
+          boosting progress in the others. For example, a healthy diet can
+          improve physical health, while mental health can improve physical
+          health, etc.
+        </p>
+        <p className="col-span-2">
+          A holistic strategy that acknowledges and addresses the synergistic
+          relationships between these three key areas is the most effective path
+          towards sustainable and comprehensive well-being. By focusing on these
+          areas, we can create a more balanced and fulfilling life. This
+          approach is the foundation of the DO-IT methodology.
+        </p>
+      </motion.div>
     </article>
   );
 };

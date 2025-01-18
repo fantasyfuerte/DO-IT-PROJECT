3192349f9a4debc3a6ce7933c9176a48fbe1beaf
fuerte <leandrofuerte75@gmail.com>
2025-01-18 17:08:03 +0100

    Refactoring

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ import ToDoApp from "@/ToDoApp/ToDoApp";
 export const HomePage: React.FC = () => {
   return (
     <>
-      <div className="flex flex-col gap-24 md:gap-6 md:grid md:grid-cols-2 md:items-center justify-center px-3 h-screen ]">
+      <div className="flex flex-col gap-24 md:gap-6 md:grid md:grid-cols-2 md:items-center justify-center px-3 h-screen">
         <ToDoApp />
         <QuotesApp />
       </div>
